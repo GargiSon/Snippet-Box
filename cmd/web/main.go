fcec31d3578622a75d3e0bc74a825180a8c26348
Gargi Son <sonigargi146@gmail.com>
2025-07-08 17:40:31 +0530

    Database Connection Setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,41 +1,66 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+	"snippetbox/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-// Define an application struct to hold the application wide dependencies for the web application. For now we'll only include the structured logger, but we'll add more to this as the build progresses.
+// Add a snippets field to the application struct. This will allow us to make the SnippetModel object Available to our handlers.
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
-	// Define a new Command Line with the name 'addr', a default value of ":4000" and some short help text explaining what the flag controls.The value of the flag will be stored in the addr.
 	addr := flag.String("addr", ":4000", "Http netwrok address")
-	// It is used to parse command line flag. This reads in the command line flag. This reads in the command line flag value and assigns it to the addr variable otherwise it will always contain the default value of :4000. If any errors will be encountered during the parsing the application will be terminated.
+
+	// Define a new command-line flag for the MYSQL DSN string.
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MYSQL data source name")
+
 	flag.Parse()
 
-	// Use the slog.New() function to initialize a new structured logger, which writes to the standard out stream and uses the default settings.
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	// Initialize a new instance of our application struct, containing the dependencies (for now, just the structured logger).
+	// To keep the main() function tidy I've put the code for creating a connection pool into the separate openDB() function below. We pass openDB() the DSN from the command-line flag.
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is closed before the main() function exists.
+	defer db.Close()
+
+	//Initialize a models.SnippetModel instance containing the connec
 	app := &application{
 		logger: logger,
 	}
 
-	// // log.Print("Starting server on :4000")
-	// log.Printf("starting server on %s", *addr)
-	// err := http.ListenAndServe(":4000", mux)
-	// log.Fatal(err)
-
-	// Use the Info() method to log the starting server message at Info severity(along with the listen address as an attribute).
 	logger.Info("Starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
-	// And we also use the Error() method to log any error message returned by http.ListenAndServe() at Error severity (with no additional attributes), and then call os.Exit(1) to terminate the application with exit code 1.
+	// Because the error variable is now already declared in the code above, we need to use the assignment operator = here, instead of the := 'declare and assign' operator.
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	//To verify everything is setup correctly we use this method to create a connection and check for any errors
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
