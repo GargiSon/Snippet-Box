4fbac49be4c92d45077132c07999ef14d9c73c8f
Gargi Son <sonigargi146@gmail.com>
2025-07-09 15:30:56 +0530

    Middleware implemented

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -30,26 +30,16 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		app.serverError(w, r, err)
 		return
 	}
-	// Initialize a new buffer.
 	buf := new(bytes.Buffer)
-	// Write the template to the buffer, instead of straight to the
-	// http.ResponseWriter. If there's an error, call our serverError() helper
-	// and then return.
 	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-	// If the template is written to the buffer without any errors, we are safe
-	// to go ahead and write the HTTP status code to http.ResponseWriter.
 	w.WriteHeader(status)
-	// Write the contents of the buffer to the http.ResponseWriter. Note: this
-	// is another time where we pass our http.ResponseWriter to a function that
-	// takes an io.Writer.
 	buf.WriteTo(w)
 }
 
-// Create an newTemplateData() helper, which returns a templateData struct initialized with the current year. Note that we're not using the *http.Request parameter here at the moment, but we will do later in the book.
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
