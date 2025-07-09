b0c20cf4319c43da0c3b82f0c079470fbf71a3c3
Gargi Son <sonigargi146@gmail.com>
2025-07-09 15:36:07 +0530

    Middleware modified

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,5 +13,10 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
 }
