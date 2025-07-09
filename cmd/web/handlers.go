2f9bf56f789699c7e23f02b187c47756a8546515
Gargi Son <sonigargi146@gmail.com>
2025-07-09 11:39:57 +0530

    Record SQL queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"snippetbox/internal/models"
 	"strconv"
@@ -13,31 +12,40 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
-		// Because the home handler is now a method against application struct it can access its fields, including the structured logger. We'll use this to create a log entry level containing the error message, also including the request method and URI as attributes to assisst with debugging.
-		// app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		// http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-
-		//These 2 lines in one
 		app.serverError(w, r, err)
 		return
 	}
-
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		// app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		// http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-
-		// These 2 lines in one
-		app.serverError(w, r, err)
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+
+	// files := []string{
+	// 	"./ui/html/base.tmpl",
+	// 	"./ui/html/partials/nav.tmpl",
+	// 	"./ui/html/pages/home.tmpl",
+	// }
+
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	// Because the home handler is now a method against application struct it can access its fields, including the structured logger. We'll use this to create a log entry level containing the error message, also including the request method and URI as attributes to assisst with debugging.
+	// 	// app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
+	// 	// http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+
+	// 	//These 2 lines in one
+	// 	app.serverError(w, r, err)
+	// 	return
+	// }
+
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	// app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
+	// 	// http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+
+	// 	// These 2 lines in one
+	// 	app.serverError(w, r, err)
+	// }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,10 +2,10 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
-// Define a Snippet type to hold the data for an individual snippet. Notice how the fields of the struct correspond to the fields in our MySQL snippets table?
 type Snippet struct {
 	ID      int
 	Title   string
@@ -14,43 +14,31 @@ type Snippet struct {
 	Expires time.Time
 }
 
-// Define a SnippetModel type which wraps a sql.DB connection pool.
 type SnippetModel struct {
 	DB *sql.DB
 }
 
-// This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	// Write the SQL statement we want to execute.
 	stmt := `INSERT INTO snippets (title, content, created, expires) VALUES(?,?,UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(),INTERVAL ? DAY))`
 
-	// Use the Exec() method on the embedded connection pool to execute the statement. The first parameter is the SQL statement, followed by the values for the placeholder parameters: title, content and expiry in that order. This method returns a sql.Result type, which contains some basic information about what happened when the statement was executed.
 	result, err := m.DB.Exec(stmt, title, content, expires)
 	if err != nil {
 		return 0, err
 	}
 
-	//Use the LastInsertID() method on the result to get the ID of out newly inserted record in the snippets table.
 	id, err := result.LastInsertID()
 	if err != nil {
 		return 0, err
 	}
-	// The ID returned has the type int64, so we convert it to an int type before returning
 	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-	//Write the SQL statement we want to execute. Again, I've split it over it over two lines for readability.
-	stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id=?`
-
-	//Use the QueryRow() method on the connection pool to execute our SQL Statement, passing in the untrusted id variable as the value for the placeholder parameter. This returns a pointer to a sql.ROw object which holds the result from the database
-	row := m.DB.QueryRow(stmt, id)
 	var s Snippet
-
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	//Instead of QueryRow() used Scan()
+	err := m.DB.QueryRow("SELECT ...", id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
-		//If the query returns no rows, then row.Scan() will return a sql.ErrNoRows error. We use the error.Is() function check for that error specifically, and return our own ErrNoRecord error instead we will create this in a moment.
-		if error.Is(err, sql.ErrNoRows) {
+		if errors.Is(err, sql.ErrNoRows) {
 			return Snippet{}, ErrNoRecord
 		} else {
 			return Snippet{}, err
@@ -59,7 +47,40 @@ func (m *SnippetModel) Get(id int) (Snippet, error) {
 	return s, nil
 }
 
-// This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-	return nil, nil
+	//Write the Sql statement we want to execute
+	stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	//Use the Query() method on the connection pool to execute our SQL statement. This returns a sql.Rows resultset containing the result of our query.
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	//We defer rows.Close() to ensure the sql.Rows resultset is always properly closed before the Latest() method returns. This defer statement should come *after* you check for an error from the Query() method. Otherwise, if Query() returns an error, you'll get an panic trying to close a nil resultset.
+	defer rows.Close()
+
+	//Initialize an empty slice to hold the snippet structs.
+	var snippets []Snippet
+
+	//Use rows.Next to iterate through the rows in the resultset. This prepares the first (add then each subsequent) row to be acted on by the rows.Scan() method. If iteration over all the rows completes then the resultset automatically closes itself and frees-up the underlying database connection.
+	for rows.Next() {
+		//Create a new zeroed Snippet struct
+		var s Snippet
+		//Use rows.Scan() to copy the values from each field in the row to the new snippet object that we created.Again, the arguments to row.Scan() must be pointers to the place you want to copy the data into, and the number of arguments must be exactly the same as the number of columns returned by your statement.
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		//Append it to the slice of snippets
+		snippets = append(snippets, s)
+	}
+
+	//When the rows.Next() loop has finished we call rows.Err() to retrieve any error that was encountered during the iteration. It's important to call this- don't assume that a successful iteration was completed over the whole resultset.
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	//If everything went OK then return the snippets slice.
+	return snippets, nil
 }
